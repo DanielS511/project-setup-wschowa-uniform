f7fc830db7f12e8744a73b3746e56c0ab29d94a2
Mingqian Sun <47837369+DanielS511@users.noreply.github.com>
2022-02-20 17:39:21 -0500

    Dockerize the app

--- back-end/routers/Home.js
+++ back-end/routers/Home.js
@@ -4,38 +4,6 @@ const axios = require("axios")
 const Post = require("../models/Post")
 
 router.get("/", async (req, res) => {
-	// this is just a function to add a post
-	// Do this before someone complete the create new post part
-	// Post.insertMany(
-	// 	[
-	// 		{
-	// 			user: {
-	// 				username: "daniel",
-	// 				gender: "male",
-	// 				// birthday: req.body.birthday,
-	// 				description: "req.body.description",
-	// 			},
-	// 			playlist_name: "name",
-	// 			playlist_link: "37i9dQZF1DWWjGdmeTyeJ6",
-	// 			caption: "hi",
-	// 			comments: [
-	// 				{
-	// 					content: "hiiiii",
-	// 					user: {
-	// 						username: "daniel",
-	// 						gender: "male",
-	// 						// birthday: req.body.birthday,
-	// 						description: "req.body.description",
-	// 					},
-	// 				},
-	// 			],
-	// 		},
-	// 	],
-	// 	function (e) {
-	// 		console.error(e)
-	// 	}
-	// )
-
 	// we want to get latest 20 posts
 	const allPosts = await Post.find({}).sort({ date: -1 }).limit(20)
 	// console.log(allPosts)
